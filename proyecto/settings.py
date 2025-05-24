fd779a4286f4c3c6cf794bff55e702f53450a77d
Nicolas Rivas Meneses <nicolasrivasmeneses23@gmail.com>
2025-05-24 14:23:23 -0500

    Todas las rutas

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-uu%mujl_sw5axans&rrbp)3k0#n49+s##($6^q#!8d%gnb!q7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 
 # Application definition
